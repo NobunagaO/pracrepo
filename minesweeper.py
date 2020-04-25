c90dfa3221e10908c34caaf9dfd7ac479e60dfbe
Nobunaga Oda <nobunaga2019k@gmail.com>
2020-04-25 13:30:09 +0900

    ref #2
    Create system.

--- minesweeper.py
+++ minesweeper.py
@@ -11,7 +11,6 @@ class Minesweeper():
     def set_bombs(self, x, y, cnt):
         cnt = (self.height * self.width - 1) if cnt >= (self.height * self.width - 1) else cnt
         self.realfield[y][x] = 2
-        self.outputfield[y][x] = 1
         while True:
             if cnt <= 0:
                 break
@@ -26,6 +25,7 @@ class Minesweeper():
                 self.numfield[i][j] = self.count_around(j, i)
                 if self.realfield[i][j] == 1:
                     self.numfield[i][j] = 9
+        self.open_function(x, y)
 
     def count_around(self, x, y):
         cnt = 0
@@ -37,8 +37,22 @@ class Minesweeper():
                     cnt += 1
         return cnt
 
-    def open_here(self, x, y):
-        self.outputfield[y][x] = 1
+    def open_function(self, x, y):
+        if self.numfield[y][x] == 0 and self.outputfield[y][x] != 1:
+            self.outputfield[y][x] = 1
+            self.open_around(x, y)
+        else:
+            self.outputfield[y][x] = 1
+
+    def open_around(self, x, y):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    continue
+                if self.numfield[y+i][x+j] != 0:
+                    self.outputfield[y+i][x+j] = 1
+                if x+j > 0 and x+j < self.width+1 and y+i > 0 and y+i < self.height+1:
+                    self.open_function(x+j, y+i)
 
     def print_field(self, field):
         for i in range(1, self.height+1):
@@ -53,7 +67,7 @@ class Minesweeper():
         while True:
             print("="*10)
             x, y = [int(i) for i in input().split()]
-            self.open_here(x, y)
+            self.open_function(x, y)
             self.output_field()
             if self.numfield[y][x] == 9:
                 break
