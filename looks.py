df744e5877a1ecad4e506913b2fd2565039083fd
Nobunaga Oda <nobunaga2019k@gmail.com>
2020-04-27 21:52:30 +0900

    ref #2
    change looks file.

--- looks.py
+++ looks.py
@@ -1,8 +1,28 @@
 import pyxel
 
-pyxel.init(120, 80)
+class App:
+    def __init__(self):
+        self.display_image = [[True for i in range(10)] for j in range(10)]
+        pyxel.init(160, 160, caption="Minesweeper")
+        pyxel.load("sample.pyxres")
+        pyxel.mouse(True)
+        pyxel.run(self.update, self.draw)
 
-while True:
-    pyxel.cls(3)
-    pyxel.rectb(pyxel.frame_count % 160 - 40, 20, 40, 40, 7)
-    pyxel.flip()
+    def update(self):
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
+            self.display_image[pyxel.mouse_y // 16][pyxel.mouse_x // 16] = False
+
+    def draw(self):
+        pyxel.cls(0)
+        #pyxel.blt(0, 0, 0, 0, 0, 16, 16)
+        for i in range(10):
+            for j in range(10):
+                if self.display_image[i][j]:
+                    pyxel.blt(16 * j, 16 * i, 0, 0, 0, 16, 16)
+        pyxel.text(16, 16, "Hello", pyxel.frame_count % 16)
+        self.draw_map()
+
+    def draw_map(self):
+        pass
+
+App()
